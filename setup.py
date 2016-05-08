5daa9f7ccbaa95d4f2a9b3b8be06cbb0dd27a895
Kosuke Kusano <cocuh.kk@gmail.com>
2016-05-09 00:09:12 +0900

    add rust module

--- setup.py
+++ setup.py
@@ -1,10 +1,28 @@
 from setuptools import setup, find_packages
+from rust_ext import build_rust, install_with_rust, RustModule
+
+
+rust_modules = [
+    RustModule(
+        'benedictine.core',
+        'core/Cargo.toml',
+    ),
+]
 
 setup(
     name='benedictine',
     version='0.0.1',
     author='cocuh',
     author_email='cocuh.kk@tsukuba.ac.jp',
+    cmdclass={
+        'build_rust': build_rust,
+        'install_lib': install_with_rust,
+    },
+    options={
+        'build_rust': {
+            'modules': rust_modules,
+        },
+    },
     packages=find_packages(),
     install_requires=['gevent'],
     test_suite='nose.collector',
