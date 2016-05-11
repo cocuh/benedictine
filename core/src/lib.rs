c8b5d5d3b740ad3a6e1370b36955fe8d2ad4c9bb
Kosuke Kusano <cocuh.kk@gmail.com>
2016-05-12 00:34:16 +0900

    run it anyway

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,5 +1,4 @@
-use std::sync::{Mutex,Arc,Condvar};
-use std::sync::mpsc::Receiver;
+use std::sync::{Mutex, Arc, Condvar};
 use std::thread;
 
 #[macro_use]
@@ -13,9 +12,7 @@ pub struct State {
 
 impl State {
     fn new(data: Vec<usize>) -> State {
-        State {
-            data: data,
-        }
+        State { data: data }
     }
     fn is_leaf(&self) -> bool {
         self.data.len() >= 3
@@ -31,23 +28,22 @@ impl std::cmp::PartialEq for State {
 
 #[derive(Debug)]
 struct StateIterator {
-    current:usize,
+    current: usize,
 }
 
 impl StateIterator {
     fn new() -> StateIterator {
-        StateIterator {
-            current:1,
-        }
+        StateIterator { current: 1 }
     }
+
     fn next(&mut self, state: &State) -> Option<State> {
         let mut data = state.data.clone();
         match self.current {
-            1...3 =>{
+            1...3 => {
                 data.push(self.current);
                 self.current += 1;
                 Some(State::new(data))
-            },
+            }
             _ => None,
         }
     }
@@ -101,22 +97,22 @@ struct Bounds {
 
 impl Bounds {
     fn new() -> Bounds {
-        Bounds{}
+        Bounds {}
     }
 }
 
-fn remove_item<T:Eq>(nodes:&mut Vec<Arc<T>>, node:&Arc<T>) {
+fn remove_item<T: Eq>(nodes: &mut Vec<Arc<T>>, node: &Arc<T>) {
     match nodes.iter().position(|n| *(*n) == **node) {
         Some(idx) => {
             nodes.remove(idx);
-        },
-        None => {},
+        }
+        None => {}
     }
 }
 
 
-fn is_all_worker_waiting(workerids: &Vec<usize>, thread_num:usize) -> bool {
-    return (0..thread_num).all(|id|{workerids.contains(&id)});
+fn is_all_worker_waiting(workerids: &Vec<usize>, thread_num: usize) -> bool {
+    return (0..thread_num).all(|id| workerids.contains(&id));
 }
 
 
@@ -137,8 +133,10 @@ pub struct Searcher {
 
 impl Searcher {
     fn new() -> Searcher {
+        let mut nodes = Vec::new();
+        nodes.push(Arc::new(Node::new(State::new(Vec::new()))));
         Searcher {
-            nodes: Arc::new(Mutex::new(Vec::new())),
+            nodes: Arc::new(Mutex::new(nodes)),
             results: Arc::new(Mutex::new(Vec::new())),
             bounds: Arc::new(Mutex::new(Bounds::new())),
             waiting_workers: Arc::new(Mutex::new(Vec::new())),
@@ -147,97 +145,108 @@ impl Searcher {
         }
     }
 
-    fn run(&self, thread_num:usize) {
-        assert!(thread_num>=1);
+    fn run(&self, thread_num: usize) {
+        assert!(thread_num >= 1);
 
-        let workers = (0..thread_num).map(|worker_id|{
-            let nodes = self.nodes.clone();
-            let results = self.results.clone();
-            let bounds = self.bounds.clone();
-            let waiting_workers = self.waiting_workers.clone();
-            let is_finished = self.is_finished.clone();
-            let condvar_worker = self.condvar_worker.clone();
+        fn push_node(condvar_worker: &Arc<Condvar>,
+                     nodes: &Arc<Mutex<Vec<Arc<Node>>>>,
+                     node: Arc<Node>) {
+            nodes.lock().unwrap().push(node);
+            condvar_worker.notify_all();
+        };
 
-            fn push_node(condvar_worker: &Arc<Condvar>, nodes: &Arc<Mutex<Vec<Arc<Node>>>>,node:Arc<Node>){
-                nodes.lock().unwrap().push(node);
-                condvar_worker.notify_all();
-            };
+        let workers = (0..thread_num)
+            .map(|worker_id| {
+                let nodes = self.nodes.clone();
+                let results = self.results.clone();
+                let bounds = self.bounds.clone();
+                let waiting_workers = self.waiting_workers.clone();
+                let is_finished = self.is_finished.clone();
+                let condvar_worker = self.condvar_worker.clone();
 
-            thread::spawn(move || {
-                debug!("[worker {}] start", worker_id);
-                'worker: loop {
-                    let mut node;
-                    'get_node: loop {
-                        let mut pop_result = nodes.lock().unwrap().pop();
-                        match pop_result {
-                            Some(n) => {
-                                node = n.clone();
-                                break 'get_node;
-                            },
-                            None => {
-                                debug!("[worker {}]no elem in queue", worker_id);
-                                waiting_workers.lock().unwrap().push(worker_id);
-                                let all_waiting = is_all_worker_waiting(&*waiting_workers.lock().unwrap(), thread_num);
-                                if all_waiting {
-                                    *is_finished.lock().unwrap() = true;
-                                    condvar_worker.notify_all();
-                                    debug!("[worker {}] finished first", worker_id);
-                                    return;
-                                } else {
-                                    condvar_worker.wait(waiting_workers.lock().unwrap());
-                                    if *is_finished.lock().unwrap() {
-                                        debug!("[worker {}] finished", worker_id);
+                thread::spawn(move || {
+                    debug!("[worker {}] start", worker_id);
+                    'worker: loop {
+                        let mut node;
+                        'get_node: loop {
+                            let mut pop_result = nodes.lock().unwrap().pop();
+                            match pop_result {
+                                Some(n) => {
+                                    node = n;
+                                    break 'get_node;
+                                }
+                                None => {
+                                    debug!("[worker {}] no elem in queue", worker_id);
+                                    waiting_workers.lock().unwrap().push(worker_id);
+                                    let all_waiting = is_all_worker_waiting(&*waiting_workers.lock()
+                                                                                .unwrap(),
+                                                                            thread_num);
+                                    if all_waiting {
+                                        *is_finished.lock().unwrap() = true;
+                                        condvar_worker.notify_all();
+                                        debug!("[worker {}] finished first", worker_id);
                                         return;
+                                    } else {
+                                        condvar_worker.wait(waiting_workers.lock().unwrap());
+                                        if *is_finished.lock().unwrap() {
+                                            debug!("[worker {}] finished", worker_id);
+                                            return;
+                                        } else {
+                                            debug!("[worker {}] re-start", worker_id);
+                                        }
                                     }
                                 }
-                            },
-                        }
-                    } // get_node and detect is finished
+                            }
+                        } // get_node and detect is finished
 
-                    { // leaf process
-                        if node.is_leaf() {
-                            let mut nodes = nodes.lock().unwrap();
-                            remove_item(&mut*nodes, &node);
-                            results.lock().unwrap().push(node);
-                            continue 'worker;
-                        } else {
-                            push_node(&condvar_worker, &nodes, node.clone());
-                        }
-                    }
-
-                    { // check bounds
-                        let bounds = bounds.lock().unwrap().clone();
-                        if !node.satisfy_bound(bounds) {
-                            continue;
-                        }
-                    }
-
-                    { // generate child
-                        debug!("next {}", worker_id);
-                        let child = node.next_child();
-                        match child {
-                            None => {
+                        {
+                            // leaf process
+                            if node.is_leaf() {
                                 let mut nodes = nodes.lock().unwrap();
-                                remove_item(&mut*nodes, &node);
-                            },
-                            Some(child) => {
-                                push_node(&condvar_worker, &nodes, Arc::new(child));
-                            },
+                                remove_item(&mut *nodes, &node);
+                                results.lock().unwrap().push(node);
+                                continue 'worker;
+                            } else {
+                                push_node(&condvar_worker, &nodes, node.clone());
+                            }
+                        }
+
+                        {
+                            // check bounds
+                            let bounds = bounds.lock().unwrap().clone();
+                            if !node.satisfy_bound(bounds) {
+                                continue;
+                            }
+                        }
+
+                        {
+                            // generate child
+                            debug!("[worker {}] next", worker_id);
+                            let child = node.next_child();
+                            match child {
+                                None => {
+                                    let mut nodes = nodes.lock().unwrap();
+                                    remove_item(&mut *nodes, &node);
+                                }
+                                Some(child) => {
+                                    push_node(&condvar_worker, &nodes, Arc::new(child));
+                                }
+                            }
                         }
                     }
-                }
+                })
             })
-        }).collect::<Vec<_>>();
+            .collect::<Vec<_>>();
+
         for worker in workers.into_iter() {
             worker.join();
         }
     }
-
 }
 
 
 mod tests {
-    use std::sync::{Mutex,Arc,Condvar};
+    use std::sync::{Mutex, Arc, Condvar};
     use super::*;
     extern crate env_logger;
 
@@ -246,8 +255,7 @@ mod tests {
         let _ = env_logger::init();
         let mut searcher = Arc::new(Searcher::new());
         println!("{:?}", searcher);
-        searcher.run(2);
+        searcher.run(8);
+        println!("{:?}", *searcher.results.lock().unwrap());
     }
 }
-
-
